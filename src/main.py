663a83e5a5730161efa643dc1d2bfb87ce8049dd
genacr0co <haratian.gena@gmail.com>
2025-07-01 13:16:01 +0500

    Add Base Guild War module routers

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from api.v1.start_endpoints import router as start
+from api.v1.guilds_war.routers import router as guild_war_router
 
 from settings import settings
 
@@ -13,3 +14,4 @@ app = FastAPI(
 )
 
 app.include_router(start, prefix="/api/v1", tags=["old"])
+app.include_router(guild_war_router, prefix="/api/v1", tags=["Guilds War"])
