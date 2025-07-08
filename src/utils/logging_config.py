25745a03779fc96601ae3bf05697244f4ec1a493
genacr0co <haratian.gena@gmail.com>
2025-07-08 12:15:11 +0500

    add ci/cd

--- src/utils/logging_config.py
+++ src/utils/logging_config.py
@@ -1,5 +1,11 @@
+import os
 import logging
 from logging.config import dictConfig
+
+# Получаем путь к директории, где лежит этот файл (logging_config.py)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+log_dir = os.path.join(BASE_DIR, "logs")
+os.makedirs(log_dir, exist_ok=True)
 
 LOGGING_CONFIG = {
     "version": 1,
@@ -18,26 +24,22 @@ LOGGING_CONFIG = {
         "file": {
             "formatter": "default",
             "class": "logging.FileHandler",
-            "filename": "logs/app.log",
+            "filename": os.path.join(log_dir, "app.log"),  # 👈 здесь путь уже корректный
             "mode": "a",
         },
     },
     "loggers": {
-        # 👇 твой логгер приложения
         "app": {
             "handlers": ["console", "file"],
             "level": "DEBUG",
         },
-
-        # 👇 SQLAlchemy — отключаем или ставим WARNING
         "sqlalchemy.engine": {
             "handlers": ["console", "file"],
-            "level": "CRITICAL",  # INFO → WARNING
+            "level": "CRITICAL",
             "propagate": False,
         },
     },
 }
-
 
 dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger("app")
