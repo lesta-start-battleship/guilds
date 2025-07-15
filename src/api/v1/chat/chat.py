b24e8705eb391cab6c8c1ba4d0fbfecc14283ee1
Eugene <talonin92@mail.ru>
2025-07-15 17:20:36 +0500

    - [fix] chat by token

--- src/api/v1/chat/chat.py
+++ src/api/v1/chat/chat.py
@@ -1,11 +1,19 @@
 from fastapi import APIRouter, WebSocket, WebSocketException, Depends
+from jose import jwt
 from sqlalchemy.ext.asyncio import AsyncSession
 from infra.db.database import get_db
 from monitoring.metrics import metrics
 from services.chat_service import handle_websocket
-from utils.validate_token import validate_token
+
+
+async def validate_token(token: str):
+    payload = jwt.decode(token, key="", options={"verify_signature": False})
+    return payload
+
+
 
 router = APIRouter()
+
 
 async def get_token_websocket(websocket: WebSocket):
     auth_header = websocket.headers.get('Authorization')
@@ -19,15 +27,17 @@ async def get_token_websocket(websocket: WebSocket):
     except ValueError:
         raise WebSocketException(code=1008, reason="Invalid Authorization header format")
 
+
 @router.websocket("/ws/guild/{guild_id}")
 async def guild_websocket(
     guild_id: int,
     websocket: WebSocket,
     db: AsyncSession = Depends(get_db),
-    token: str = Depends(get_token_websocket),  # Изменяем аннотацию типа на str
+    token: str = Depends(get_token_websocket),  # Принимаем строку токена
 ):
     await websocket.accept()
     metrics.websocket_connections_total.labels(path=str(websocket.url.path)).inc()
     payload = await validate_token(token)  # Передаем строку токена
     user_id = int(payload["sub"])
     await handle_websocket(guild_id, user_id, websocket, db)
+
